c789d520c30c1690a03f6976f239bed81bd5cb76
ichernya <vanya.chernyak2@gmail.com>
2021-07-17 14:41:35 -0700

    tables

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -2,3 +2,6 @@
 INSERT INTO dummy (created) VALUES (current_timestamp);
 
 -- Populate Your Tables Here --
+DELETE FROM users;
+INSERT INTO users (user pass firstName lastName) VALUES ('user1', '1234', 'Alex', 'Jacobs');
+INSERT INTO users (user pass firstName lastName) VALUES ('user2', '1235', 'Evan', 'Crow');
--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -3,3 +3,15 @@ DROP TABLE IF EXISTS dummy;
 CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
 -- Your database schema goes here --
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    user VARCHAR(32),
+    pass VARCHAR(32),
+    firstName VARCHAR(32),
+    lastName VARCHAR(32)
+    );
+
+DROP TABLE IF EXISTS channel;
+CREATE TABLE channel(
+    messages VARCHAR(256);
+    );
