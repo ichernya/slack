454b3a34858c6551b3e1dd898c4c6190676a06c9
Keaton Singer <keatonsinger@gmail.com>
2021-07-17 15:26:48 -0700

    Login Page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import './Login.css';
 
 /**
 * Simple component with no state.
@@ -9,19 +11,35 @@ function Login() {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
   return (
-    <div>
-      <input
-        type='text'
-        onInput={(event)=>setUsername(event.target.value)}
-        value={username}
-        placeholder='username'
-      />
-      <input
-        type='text'
-        onInput={(event)=>setPassword(event.target.value)}
-        value={password}
-        placeholder='password'
-      />
+    <div id='login-container'>
+      <div id='login-text'>
+        Login
+      </div>
+      <div id='username-container'>
+        <input
+          id = 'username-input'
+          type='text'
+          onInput={(event)=>setUsername(event.target.value)}
+          value={username}
+          placeholder='Username'
+        />
+      </div>
+      <div id='password-container'>
+        <input
+          id = 'password-input'
+          type='text'
+          onInput={(event)=>setPassword(event.target.value)}
+          value={password}
+          placeholder='Password'
+        />
+      </div>
+      <div>
+        <span id='remember-container'>
+          <span id='checkbox'><Checkbox color='white'/></span>
+          <span id='remember-text'>Remember me</span>
+        </span>
+        <button id='sign-button'>Sign in</button>
+      </div>
     </div>
   );
 }
