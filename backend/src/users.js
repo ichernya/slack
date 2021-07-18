0744d0fdf3cc9c48de173904347f29f354bcfd40
Keaton Singer <keatonsinger@gmail.com>
2021-07-18 13:17:38 -0700

    Username Stuff

--- backend/src/users.js
+++ backend/src/users.js
@@ -9,23 +9,22 @@ const pool = new Pool({
 });
 
 const findUser = async (user, pass) => {
-    console.log(user, pass);
-    const select = 'SELECT * FROM users WHERE username = $1 AND passhash = $2';
+    const select = `SELECT * FROM users WHERE username = $1 AND passhash = $2`;
     const query = {
         text: select,
         values: [ user, pass ],
     }
-    const person = await pool.query(query)
+    const {person} = await pool.query(query)
     console.log(person);
     if (person) {return person;}
     else {return undefined;}
 };;
 
 exports.login = async (req, res) => {
-    //grab user from frontend instead
-    const user = 'user1';
-    const pass = '1234';
-    const person = await findUser(user, pass);
+    const usernameQuery = req.query.username;
+    const passwordQuery = req.query.password;
+    console.log(usernameQuery, passwordQuery)
+    const person = await findUser(usernameQuery, passwordQuery);
     if (person) {res.status(200).send();}
     else {res.status(404).send();}
 }
